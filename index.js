7b7c9dc6917718a3568cb2338b55547d624c879d
SlemJosh <joshua.slem@gmail.com>
2023-10-24 12:43:58 -0600

    Added a function to display the badges for the licenses we listed on our questions.

--- index.js
+++ index.js
@@ -9,139 +9,122 @@ const generateMarkdown = require('./utils/generateMarkdown'); // custom module t
 
 // TODO: Create an array of questions for user input
 const questions = [
-
-// title
-{
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?'
-    validate: function (input){
-        return input !== '' ? true : 'Title cannot be left empty.';
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: function (input) {
+            return input !== '' ? true : 'Title cannot be left empty.';
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description about your project. Try to answer some of the following questions. Why did you make it? For whom did you make it for? What does your project do?',
+        validate: function (input) {
+            return input !== '' ? true : 'Description cannot be left empty.';
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+        validate: function (input) {
+            return input !== '' ? true : 'Installation steps cannot be empty. You may type N/A if not applicable';
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the steps for using the project?',
+        validate: function (input) {
+            return input !== '' ? true : 'Usage steps cannot be empty. You may type N/A if not applicable';
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license does your project use?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'Other', 'None']
+    },
+    {
+        type: 'input',
+        name: 'customLicense',
+        message: 'Please enter the license your project uses.',
+        when: (answers) => answers.license === 'Other',
+        validate: function (input) {
+            return input !== '' ? true : 'Custom license cannot be empty. You may type N/A if not applicable';
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'List your collaborators or any sources that helped you create this project.',
+        validate: function (input) {
+            return input !== '' ? true : 'Contributors/source list cannot be empty. You may type N/A if not applicable';
+        }
+    },
+    {
+        type: 'list',
+        name: 'testsRequired',
+        message: 'Is there a test needed for this project?',
+        choices: ['Yes', 'No']
+    },
+    {
+        type: 'input',
+        name: 'testingSteps',
+        message: 'Please specify the testing steps:',
+        when: (answers) => answers.testsRequired === 'Yes',
+        validate: function (input) {
+            return input !== '' ? true : 'Tests required cannot be empty. You may type N/A if not applicable';
+        }
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?',
+        validate: function (input) {
+            return input !== '' ? true : 'Username cannot be empty. You may type N/A if not applicable';
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: function (input) {
+            return input !== '' ? true : 'Email cannot be empty. You may type N/A if not applicable';
+        }
     }
-},
-
-// description
-{
-    type: 'input',
-    name: 'description',
-    message: 'Write a description about your project. Try to answer some of the following questions. Why did you make it? For Whom did you make it for? What does your project do?'
-    validate: function (input){
-        return input !== '' ? true : 'Description cannot be left empty.';
-    }
-},
-
-
-// installation
-{
-    type: 'input',
-    name: 'installation',
-    message: 'What are the steps required to install your project?'
-    validate: function (input) {
-        return input !== '' ? true : 'Installation steps cannot be empty. You may type N/A if not applicable';
-    }
-},
-
-// usage
-{
-    type: 'input',
-    name: 'usage',
-    message: 'What are the steps for using the project?'
-    validate: function (input) {
-        return input !== '' ? true : 'Usage steps cannot be empty. You may type N/A if not applicable';
-    }
-},
-
-// license
-{
-    type: 'list',
-    name: 'license',
-    message: 'What type of license does your project use?',
-    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'Other', 'None']
-},
-
-// custom license
-{
-    type: 'input',
-    name: 'customLicense'
-    message: 'Please enter the license your project uses.', 
-    when: (answers) => answers.license === 'Other',
-    validate: function (input){
-        return input !== '' ? true : 'Custom license canot be empty. You may type N/A if not applicable';
-    }
-
-},
-
-// contributing
-{
-    type: 'input',
-    name: 'contribution',
-    message: 'List your collaborators or any sources that helped you create this project.'
-    validate: function (input) {
-        return input !== '' ? true : 'Usage steps cannot be empty. You may type N/A if not applicable';
-    }
-},
-
-// test required
-{
-    type: 'input',
-    name: 'testsRequired',
-    message: 'Is there a test needed for this project?'
-    choices: ['Yes', 'No'];
-
-},
-
-// testing steps
-{
-    type: 'input',
-    name: 'testingSteps',
-    message: 'Please specify the testing steps:',
-    when: (answers) => answers.testsRequired === 'Yes',
-    validate: function (input) {
-        return input !== '' ? true : 'Tests required cannot be empty. You may type N/A if not applicable';
-    }
-},
-
-// questions
-
-// github username
-{
-    type: 'input',
-    name: 'username',
-    message: 'What is your github username?'
-    validate: function (input) {
-        return input !== '' ? true : 'Username cannot be empty. You may type N/A if not applicable';
-    }
-},
-// email
-{
-    type: 'input',
-    name: 'email',
-    message: 'What is your email?'
-    validate: function (input) {
-        return input !== '' ? true : 'Email cannot be empty. You may type N/A if not applicable';
-    }
-}
 ];
 
-// Test function to check answers and validation of questions and answers.
-function testQuestions(){
-    inquirer
-    .prompt(questions)
-    .then((answers) => {
-        console.log('User Responses:');
-        console.log(answers);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
+// function testQuestions() {
+//     inquirer
+//         .prompt(questions)
+//         .then((answers) => {
+//             console.log('User Responses:');
+//             console.log(answers);
+//         })
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         });
+// }
+
+// Call the testing function
+// testQuestions();
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    const filename = './README.md';
+
+    fs.writeFile('README.md', data, function (err){
+        err ? console.log(err) : console.log(filename + 'created!')
     });
 }
 
-testQuestions();
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
+// TODO: Create a function to initialize app
+function init() {
+inquirer.prompt(questions)
+.then (answers => writeToFile(generateMarkdown(answers)))
+}
 // Function call to initialize app
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+} else if (license === 'APACHE 2.0') {
+    return '![License: APACHE 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+} else if (license === 'GPL 3.0') {
+    return '![License: GPL 3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)';
+} else {
+    return ''; // Return an empty string if no valid license is provided
+}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
