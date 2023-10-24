51d53a3857201731e2e6ededc42ca2776ccf1778
SlemJosh <joshua.slem@gmail.com>
2023-10-24 12:27:11 -0600

    Added validation for all the questions. Also added two additional questions based on answers given to 2 of the questions already asked.  If they answer a question in a certain way that requires an additional question and answer section, one will be provided for them.

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const questions = [
     type: 'input',
     name: 'title',
     message: 'What is the title of your project?'
+    validate: function (input){
+        return input !== '' ? true : 'Title cannot be left empty.';
+    }
 },
 
 // description
@@ -22,6 +25,9 @@ const questions = [
     type: 'input',
     name: 'description',
     message: 'Write a description about your project. Try to answer some of the following questions. Why did you make it? For Whom did you make it for? What does your project do?'
+    validate: function (input){
+        return input !== '' ? true : 'Description cannot be left empty.';
+    }
 },
 
 
@@ -30,6 +36,9 @@ const questions = [
     type: 'input',
     name: 'installation',
     message: 'What are the steps required to install your project?'
+    validate: function (input) {
+        return input !== '' ? true : 'Installation steps cannot be empty. You may type N/A if not applicable';
+    }
 },
 
 // usage
@@ -37,6 +46,9 @@ const questions = [
     type: 'input',
     name: 'usage',
     message: 'What are the steps for using the project?'
+    validate: function (input) {
+        return input !== '' ? true : 'Usage steps cannot be empty. You may type N/A if not applicable';
+    }
 },
 
 // license
@@ -47,33 +59,71 @@ const questions = [
     choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'Other', 'None']
 },
 
+// custom license
+{
+    type: 'input',
+    name: 'customLicense'
+    message: 'Please enter the license your project uses.', 
+    when: (answers) => answers.license === 'Other',
+    validate: function (input){
+        return input !== '' ? true : 'Custom license canot be empty. You may type N/A if not applicable';
+    }
+
+},
+
 // contributing
 {
     type: 'input',
     name: 'contribution',
     message: 'List your collaborators or any sources that helped you create this project.'
+    validate: function (input) {
+        return input !== '' ? true : 'Usage steps cannot be empty. You may type N/A if not applicable';
+    }
 },
 
-// test
+// test required
 {
     type: 'input',
-    name: 'tests',
+    name: 'testsRequired',
     message: 'Is there a test needed for this project?'
+    choices: ['Yes', 'No'];
+
+},
+
+// testing steps
+{
+    type: 'input',
+    name: 'testingSteps',
+    message: 'Please specify the testing steps:',
+    when: (answers) => answers.testsRequired === 'Yes',
+    validate: function (input) {
+        return input !== '' ? true : 'Tests required cannot be empty. You may type N/A if not applicable';
+    }
 },
 
 // questions
+
+// github username
 {
     type: 'input',
     name: 'username',
     message: 'What is your github username?'
+    validate: function (input) {
+        return input !== '' ? true : 'Username cannot be empty. You may type N/A if not applicable';
+    }
 },
-
+// email
 {
     type: 'input',
     name: 'email',
     message: 'What is your email?'
+    validate: function (input) {
+        return input !== '' ? true : 'Email cannot be empty. You may type N/A if not applicable';
+    }
 }
 ];
+
+// Test function to check answers and validation of questions and answers.
 function testQuestions(){
     inquirer
     .prompt(questions)
