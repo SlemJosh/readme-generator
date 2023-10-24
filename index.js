a230ee69b600400e6f6aeded0b006c483a56b273
SlemJosh <joshua.slem@gmail.com>
2023-10-24 12:09:35 -0600

    Added a test to check the answers and make sure the prompts were going through.

--- index.js
+++ index.js
@@ -14,30 +14,30 @@ const questions = [
 {
     type: 'input',
     name: 'title',
-    message: 'What is the title of your project?',
-}
+    message: 'What is the title of your project?'
+},
 
 // description
 {
     type: 'input',
     name: 'description',
-    message: 'Write a description about your project. Try to answer some of the following questions. Why did you make it? For Whom did you make it for? What does your project do?',
-}
+    message: 'Write a description about your project. Try to answer some of the following questions. Why did you make it? For Whom did you make it for? What does your project do?'
+},
 
 
 // installation
 {
     type: 'input',
     name: 'installation',
-    message: 'What are the steps required to install your project?',
-}
+    message: 'What are the steps required to install your project?'
+},
 
 // usage
 {
     type: 'input',
     name: 'usage',
-    message: 'What are the steps for using the project?',
-}
+    message: 'What are the steps for using the project?'
+},
 
 // license
 {
@@ -45,40 +45,53 @@ const questions = [
     name: 'license',
     message: 'What type of license does your project use?',
     choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'Other', 'None']
-}
+},
 
 // contributing
 {
     type: 'input',
     name: 'contribution',
-    message: 'List your collaborators or any sources that helped you create this project.',
-}
+    message: 'List your collaborators or any sources that helped you create this project.'
+},
 
 // test
 {
     type: 'input',
     name: 'tests',
-    message: 'Is there a test needed for this project?',
-}
+    message: 'Is there a test needed for this project?'
+},
 
 // questions
 {
     type: 'input',
     name: 'username',
-    message: 'What is your github username?',
-}
+    message: 'What is your github username?'
+},
 
 {
     type: 'input',
     name: 'email',
-    message: 'What is your email?',
+    message: 'What is your email?'
 }
 ];
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function testQuestions(){
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log('User Responses:');
+        console.log(answers);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+testQuestions();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// Function call to initialize app
+// init();
