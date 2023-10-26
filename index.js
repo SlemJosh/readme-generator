ee92ff1ac43756af1e4112ba21262123f74f9d26
SlemJosh <joshua.slem@gmail.com>
2023-10-25 21:43:01 -0600

    Fixed some typos that broke everything.

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const generateMarkdown = require('./utils/generateMarkdown'); // custom module t
 
 
 const questions = [
-    // title/N/Ame of project
+    // title/name of project
     {
         type: 'input',
-        Name: 'title',
+        name: 'title',
         message: 'What is the title of your project?',
         validate: function (input) {
             return input !== '' ? true : 'Title cannot be left empty.';
@@ -20,7 +20,7 @@ const questions = [
     // project description
     {
         type: 'input',
-        Name: 'description',
+        name: 'description',
         message: 'Write a description about your project. Try to answer some of the following questions. Why did you make it? For whom did you make it for? What does your project do?',
         validate: function (input) {
             return input !== '' ? true : 'Description cannot be left empty.';
@@ -30,27 +30,27 @@ const questions = [
     // steps for installing the project
     {
         type: 'input',
-        Name: 'installation',
+        name: 'installation',
         message: 'What are the steps required to install your project? Is there a web address to view this project?',
         validate: function (input) {
-            return input !== '' ? true : 'Installation steps cannot be empty. You may type N/A if not applicable';
+            return input !== '' ? true : 'Installation steps cannot be empty. You may type none or N/A if not applicable';
         }
     },
 
     // how to use the project steps
     {
         type: 'input',
-        Name: 'usage',
+        name: 'usage',
         message: 'What are the steps for using the project?',
         validate: function (input) {
-            return input !== '' ? true : 'Usage steps cannot be empty. You may type N/A if not applicable';
+            return input !== '' ? true : 'Usage steps cannot be empty. You may type none or N/A if not applicable';
         }
     },
 
     // license
     {
         type: 'list',
-        Name: 'license',
+        name: 'license',
         message: 'What type of license does your project use?',
         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'Other', 'None']
     },
@@ -58,28 +58,28 @@ const questions = [
     // license not specified in previous question
     {
         type: 'input',
-        Name: 'customLicense',
+        name: 'customLicense',
         message: 'Please enter the license your project uses.',
         when: (answers) => answers.license === 'Other',
         validate: function (input) {
-            return input !== '' ? true : 'Custom license cannot be empty. You may type N/A if not applicable';
+            return input !== '' ? true : 'Custom license cannot be empty. You may type none or N/A if not applicable';
         }
     },
 
     // contributors and resources cited
     {
         type: 'input',
-        Name: 'contribution',
+        name: 'contribution',
         message: 'List your collaborators or any sources that helped you create this project.',
         validate: function (input) {
-            return input !== '' ? true : 'Contributors/source list cannot be empty. You may type N/A if not applicable';
+            return input !== '' ? true : 'Contributors/source list cannot be empty. You may type none or N/A if not applicable';
         }
     },
 
     // Tests Required (Still not sure what this is for)
     {
         type: 'list',
-        Name: 'testsRequired',
+        name: 'testsRequired',
         message: 'Is there a test needed for this project?',
         choices: ['Yes', 'No']
     },
@@ -87,44 +87,44 @@ const questions = [
     // If they answer Yes to the previous, here is where they can display those steps.
     {
         type: 'input',
-        Name: 'testingSteps',
+        name: 'testingSteps',
         message: 'Please specify the testing steps:',
         when: (answers) => answers.testsRequired === 'Yes',
         validate: function (input) {
-            return input !== '' ? true : 'Tests required cannot be empty. You may type N/A if not applicable';
+            return input !== '' ? true : 'Tests required cannot be empty. You may type none or N/A if not applicable';
         }
     },
 
-    // github userN/Ame
+    // github username
     {
         type: 'input',
-        Name: 'userN/Ame',
-        message: 'What is your github userN/Ame?',
+        name: 'username',
+        message: 'What is your github username?',
         validate: function (input) {
-            return input !== '' ? true : 'UserN/Ame cannot be empty. You may type N/A if not applicable';
+            return input !== '' ? true : 'Username cannot be empty. You may type none or N/A if not applicable';
         }
     },
 
     // email
     {
         type: 'input',
-        Name: 'email',
+        name: 'email',
         message: 'What is your email?',
         validate: function (input) {
-            return input !== '' ? true : 'Email cannot be empty. You may type N/A if not applicable';
+            return input !== '' ? true : 'Email cannot be empty. You may type none or N/A if not applicable';
         }
     }
 ];
 
-// Function to write the file to a location desigN/Ated.
-function writeToFile(filename, data) {
+// Function to write the file to a location designated.
+function writeToFile(fileName, data) {
     const filename = 'README.md';
 
     fs.writeFile('./created/README.md', data, function (err){
         if (err) {
             console.error('Error writing file:', err);
         } else {
-            console.log(fileN/Ame + ' created! Check the created folder.');
+            console.log(filename + ' created! Check the created folder.');
         }
     });
 }
